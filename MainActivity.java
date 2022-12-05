b1619d3b0e615cb4b06c98c24ade3f2827c78786
mohammad-adan <119883138+mohammad-adan@users.noreply.github.com>
2022-12-05 22:47:06 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.unitconvert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+        EditText editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"👅",Toast.LENGTH_SHORT).show();
+                int kg = Integer.parseInt(editText.getText().toString());
+                double pound = 2.205 * kg;
+                textView.setText("The corresponding pound value is " + pound);
+
+            }
+        });
+    }
+}
